c248de470a4e2512fb88df212650919ee0af74a1
Shut Kirill <kyrylo.shut@onlyplay.net>
2026-05-25 11:37:51 +0100

    fixed assetsConfig files url

--- src/configs/assetsConfig.js
+++ src/configs/assetsConfig.js
@@ -1,22 +1,22 @@
 export const assetsConfig = [
-  { alias: 'black_cell', src: '/assets/black_cell.png' },
-  { alias: 'white_cell', src: '/assets/white_cell.png' },
+  { alias: 'black_cell', src: './assets/black_cell.png' },
+  { alias: 'white_cell', src: './assets/white_cell.png' },
 
-  { alias: 'pawn_figure_white', src: '/assets/figures/pawn_white.svg' },
-  { alias: 'pawn_figure_black', src: '/assets/figures/pawn_black.svg' },
+  { alias: 'pawn_figure_white', src: './assets/figures/pawn_white.svg' },
+  { alias: 'pawn_figure_black', src: './assets/figures/pawn_black.svg' },
 
-  { alias: 'rook_figure_white', src: '/assets/figures/rook_white.svg' },
-  { alias: 'rook_figure_black', src: '/assets/figures/rook_black.svg' },
+  { alias: 'rook_figure_white', src: './assets/figures/rook_white.svg' },
+  { alias: 'rook_figure_black', src: './assets/figures/rook_black.svg' },
 
-  { alias: 'knight_figure_white', src: '/assets/figures/knight_white.svg' },
-  { alias: 'knight_figure_black', src: '/assets/figures/knight_black.svg' },
+  { alias: 'knight_figure_white', src: './assets/figures/knight_white.svg' },
+  { alias: 'knight_figure_black', src: './assets/figures/knight_black.svg' },
 
-  { alias: 'bishop_figure_white', src: '/assets/figures/bishop_white.svg' },
-  { alias: 'bishop_figure_black', src: '/assets/figures/bishop_black.svg' },
+  { alias: 'bishop_figure_white', src: './assets/figures/bishop_white.svg' },
+  { alias: 'bishop_figure_black', src: './assets/figures/bishop_black.svg' },
 
-  { alias: 'queen_figure_white', src: '/assets/figures/queen_white.svg' },
-  { alias: 'queen_figure_black', src: '/assets/figures/queen_black.svg' },
+  { alias: 'queen_figure_white', src: './assets/figures/queen_white.svg' },
+  { alias: 'queen_figure_black', src: './assets/figures/queen_black.svg' },
 
-  { alias: 'king_figure_white', src: '/assets/figures/king_white.svg' },
-  { alias: 'king_figure_black', src: '/assets/figures/king_black.svg' },
+  { alias: 'king_figure_white', src: './assets/figures/king_white.svg' },
+  { alias: 'king_figure_black', src: './assets/figures/king_black.svg' },
 ];
