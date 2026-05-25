58ff3156867d46fc9ced87e3d7c0b3f7e6e9e056
Shut Kirill <kyrylo.shut@onlyplay.net>
2026-05-25 11:23:20 +0100

    moved ejs to html

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -79,7 +79,7 @@ export default (_env, argv) => {
 
       // Make an index.html from the template
       new HtmlWebpackPlugin({
-        template: './index.ejs',
+        template: './index.html',
         hash: true,
         minify: false,
       }),
